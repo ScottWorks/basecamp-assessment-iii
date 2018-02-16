9bdb05f4ae8e1af0abe47d098b803d5169a02133
ScottWorks <scottworks.io@gmail.com>
2018-02-16 15:48:52 -0500

    added lifecycle to clock

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>The current time is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(
-    element, 
-    document.getElementById('root')
-  );
+class Clock extends React.Component{
+  constructor(props){ // provides state with initial data from props
+    super(props); // gives props access to parent class
+    this.state={date: new Date()};
+  }
+  
+  componentDidMount(){ // starts timer after component is rendered in DOM
+    this.timerID=setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  componentWillUnmount(){ // tears down timer, frees unused resources
+    clearInterval(this.timerID);
+  }
+
+  tick(){
+    this.setState((previousState, props) => {
+      return {date: new Date()};
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
 }
 
-setInterval(tick, 1000);
+
+ReactDOM.render(
+  <Clock/>,
+  document.getElementById('root')
+);
